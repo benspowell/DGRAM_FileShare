ec669d7f4f46d71e159f2fbbc7815215363e354b
Ben Powell <powell.907@osu.edu>
2019-11-17 13:03:44 -0500

    update client

--- client.py
+++ client.py
@@ -4,8 +4,20 @@ import socket
 HOST = 'file.powell.mx'   # The remote host
 PORT = 5000           # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but not stream
-msg = 'Hello, world.'
-s.sendto(msg, (HOST, PORT))
-data = s.recv(1024)
+
+command = raw_input("what is your command? ")
+
+if (command=="send"):
+    recipient = raw_input("to whom? ")
+    message = raw_input("what do you want to say? ")
+    s.sendto(message, (recipient, PORT))
+elif (command=="recieve"):
+    while True:  
+        data, addr = s.recvfrom(1024)  
+        print "Received ->", data
+        print "from ", addr
+        break
+
 s.close()
-print "Received -> ", repr(data)
+
+
