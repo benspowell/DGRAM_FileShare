d439c9ab289165352b47911cd519f1099d214fe9
Ben Powell <powell.907@osu.edu>
2019-11-17 13:50:21 -0500

    structure

--- client/app.py
+++ client/app.py
@@ -1,5 +1,6 @@
-# Echo client program
 import socket
+from os import listdir
+from os.path import isfile, join
 
 PORT = 1998           # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but not stream
@@ -7,13 +8,15 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but no
 print "Welcome!"
 server_addr = raw_input("where is your filing cabinet? ")
 
+msg = ""
+recipient = ""
 
-command = raw_input("what is your header? ")
+command = raw_input("what do you want to do?: ")
 
 if (command=="send"):
+    msg = raw_input("what do you want to say? ")
     recipient = raw_input("to whom? ")
-    message = raw_input("what do you want to say? ")
-    s.sendto(message, (recipient, PORT))
+    s.sendto(msg, (recipient, PORT))
 elif (command=="recieve"):
     while True:  
         s.bind(('', PORT))
@@ -23,9 +26,18 @@ elif (command=="recieve"):
         s.close
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         break
+elif (command == "register"):
+    msg = "iam\n" + raw_input("who are you? ")
+    # recipient = server_addr
+    # s.sendto(msg, (recipient, PORT))
+    # response = s.recv(1024)
+
+    print "in your local filesystem, you have a folder called MyDrawer."
+    print "all files in your drawer will be shared with the cabinet directory."
+    files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
+    print "your files: ", files
 
 
 
 s.close()
-
 
