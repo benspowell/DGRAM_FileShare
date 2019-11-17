b824b8213bcdb37e745c1fa16946f738032f3a0c
Ben Powell <powell.907@osu.edu>
2019-11-17 14:09:46 -0500

    update client

--- client/app.py
+++ client/app.py
@@ -11,23 +11,23 @@ server_addr = raw_input("where is your filing cabinet? ")
 msg = ""
 recipient = ""
 
-    msg = "iam\n" + raw_input("who are you? ")
-    # recipient = server_addr
-    # s.sendto(msg, (recipient, PORT))
-    # response = s.recv(1024)
+msg = "iam\n" + raw_input("who are you? ")
+# recipient = server_addr
+# s.sendto(msg, (recipient, PORT))
+# response = s.recv(1024)
 
-    print "\nIn your local filesystem, you have a folder called MyDrawer."
-    print "\nAll files in your drawer will be shared with the cabinet directory.\n"
-    files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
+print "\nIn your local filesystem, you have a folder called MyDrawer."
+print "\nAll files in your drawer will be shared with the cabinet directory.\n"
+files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
 
-    print "your files: ", files
-    raw_input("\nwhen you're ready to share, press enter...")
-    
-    filestring=""
-    for f in files 
-        filestring += f + "\\"
-    msg = "ihave\n" + filestring + "/"
-    print msg
+print "your files: ", files
+raw_input("\nwhen you're ready to share, press enter...")
+
+filestring=""
+for f in files 
+    filestring += f + "\\"
+msg = "ihave\n" + filestring + "/"
+print msg
 
 while True:
     command = raw_input("what do you want to do?: ")
