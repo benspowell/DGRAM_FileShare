c8702c8eee5e7a17f6c134f36e75b28bdac0e697
Ben Powell <powell.907@osu.edu>
2019-11-17 12:03:10 -0500

    update host

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 # Echo client program
 import socket
 
-HOST = 'localhost'   # The remote host
+HOST = 'file.powell.mx'   # The remote host
 PORT = 5000           # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but not stream
 msg = 'Hello, world.'
