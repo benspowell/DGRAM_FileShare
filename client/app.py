077df282a47daeb3cfd8eb97eef4b7b051c4c909
Ben Powell <powell.907@osu.edu>
2019-11-18 18:28:26 -0500

    updates

--- client/app.py
+++ client/app.py
@@ -12,9 +12,10 @@ msg = ""
 recipient = ""
 
 msg = "iam\n" + raw_input("who are you? ")
-# recipient = server_addr
-# s.sendto(msg, (recipient, PORT))
-# response = s.recv(1024)
+recipient = server_addr
+s.sendto(msg, (recipient, PORT))
+response = s.recv(1024)
+print response
 
 print "\nIn your local filesystem, you have a folder called MyDrawer."
 print "\nAll files in your drawer will be shared with the cabinet directory.\n"
@@ -30,22 +31,71 @@ msg = "ihave\n" + filestring + "/"
 print msg
 
 while True:
-    command = raw_input("what do you want to do?: ")
+    print "Availiable options:"
+    print "     l (list): ask the cabinet directory for a list of availiable files."
+    print "     s (search): check for a certain file in the cabinet."
+    print "     g (get): get a file from a drawer."
+    print "     u (update): update your drawer, sharing any new files with the cabinet."
+    print "     o (open): open your drawer, allowing people to get files from you."
+    
+    command = raw_input("\n     >")
 
-    if (command=="send"):
-        msg = raw_input("what do you want to say? ")
-        recipient = raw_input("to whom? ")
-        s.sendto(msg, (recipient, PORT))
-    elif (command=="recieve"):
-        while True:  
+    if (command=="o"):
+        while True:
+            print "your drawer is now open for other clients to get your files"
+            print "type 'close' to close your drawer."
+
             s.bind(('', PORT))
-            data,addr = s.recv(1024)  
-            print "Received ->", data
-            print "from ", addr
+            data,addr = s.recvfrom(1024)  
+
             s.close
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            
+            msg = "take\n"+"length\n"+"filedata"
+            recipient = addr
+            s.sendto(msg, (recipient, PORT))
+            print "sent file x to ", addr
+
             break
+    elif (command == "l"):
+        msg = "list"
+        recipient = server_addr
+        s.sendto(msg, (recipient, PORT))
 
+        response = s.recv(1024)
+        print response 
+    elif (command == "s"):
+        msg = "doyouhave\n" + raw_input("what filename do you want to check for?")
+        recipient = server_addr
+        s.sendto(msg, (recipient, PORT))
 
+        response = s.recv(1024)
+        print response
+    elif (command == "g"):
+        msg = "whereis\n" + raw_input("what filename do you want to get")
+        recipient = server_addr
+        s.sendto(msg, (recipient, PORT))
+
+        response = s.recv(1024)
+        print response
+    elif (command == "u"):
+        print "\nAll files in your drawer will be shared with the cabinet directory.\n"
+        files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
+        
+        filestring=""
+        for f in files:
+            filestring += f + "\\"
+
+        print "your files: ", filestring
+        raw_input("\nwhen you're ready to share, press enter...")
+
+        
+        msg = "ihave\n" + filestring + "/"
+        recipient = server_addr
+        s.sendto(msg, (recipient, PORT))
+
+        response = s.recv(1024)
+        print response
+        
 s.close()
 
