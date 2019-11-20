02abf0d7eea662e69c2c894bb4c1f8181de8a00c
Ben Powell <powell.907@osu.edu>
2019-11-19 19:37:42 -0500

    cleanup and incremental updates

--- client/app.py
+++ client/app.py
@@ -3,39 +3,30 @@ from os import listdir
 from os.path import isfile, join
 
 HOST= ''
-PORT = 1998           # The same port as used by the server
+PORT = 1998            # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP uses Datagram, but not stream
 s.bind((HOST, PORT))
 
+# Get server address
 print "Welcome!"
 server_addr = raw_input("where is your filing cabinet? ")
 
-msg = ""
-recipient = ""
+# Register user with server
+print "ok, registering you with "+server_addr+"\n"
+msg = "iam\n" + raw_input("     who are you? ")
+sendthis(s, msg, server_addr)
+recvresp(s)
 
-msg = "iam\n" + raw_input("who are you? ")
-recipient = server_addr
-s.sendto(msg, (recipient, PORT))
-response = s.recv(1024)
-print response
-
+# Collect files
 print "\nIn your local filesystem, you have a folder called MyDrawer."
 print "\nAll files in your drawer will be shared with the cabinet directory.\n"
-files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
+filestring = collectFiles()
+print "your files: ", filestring
 
-print "your files: ", files
+# Send file names to server
 raw_input("\nwhen you're ready to share, press enter...")
-
-filestring=""
-for f in files:
-    filestring += f + "\\"
-msg = "ihave\n" + filestring + "/"
-
-#message logging
-print "\n------sending------"
-print msg
-print "-------------------\n"
-#message logging
+sendthis(s, "ihave\n" + filestring, server_addr)
+recvresp(s)
 
 while True:
     print "\nAvailiable options:"
@@ -44,125 +35,69 @@ while True:
     print "     g (grab): get a file from a drawer."
     print "     u (update): update your drawer, sharing any new files with the cabinet."
     print "     o (open): open your drawer, allowing people to get files from you."
-    
     command = raw_input("\n     >")
     print
 
     if (command=="o"): # OPEN COMMAND: waits for another user to request a file
+        inputs = [serv, sys.stdin]
         while True:
-            print "your drawer is now open for other clients to get your files"
+            print "your drawer is now open for other clients to get your files!"
             print "type 'close' to close your drawer."
 
-            response, addr = s.recvfrom(1024)  
-            #message logging
-            print "\n-----recieved------"
-            print response 
-            print "-------------------\n"
-            #message logging
+            addr = recvresp(s)
+            sendthis(s, "take\n"+"length\n"+"filedata", addr)
 
-            # s.close
-            # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            
-            msg = "take\n"+"length\n"+"filedata"
-            recipient = addr
-          
-	    #message logging
-            print "\n-----sending------"
-            print msg
-            print "-------------------\n"
-            #message logging
-
-
-	    s.sendto(msg, recipient)
-            print "sent file x to ", addr
             break
     elif (command == "l"): # LIST COMMAND: ask server to list files
-        msg = "list"
-        recipient = server_addr
+        sendthis(s, "list", server_addr)
+        recvresp(s)
 
-        #message logging
-        print "\n-----sending------"
-        print msg
-        print "-------------------\n"
-        #message logging
-
-        s.sendto(msg, (recipient, PORT))
-
-        response = s.recv(1024)
-
-        #message logging
-        print "\n-----recieved------"
-        print response 
-        print "-------------------\n"
-        #message logging
     elif (command == "s"): # SEARCH COMMAND: ask server to search for a certain file
-        msg = "doyouhave\n" + raw_input("what filename do you want to check for?")
-        recipient = server_addr
+        msg = "doyouhave\n" + raw_input("what filename do you want to check for? ")
+        sendthis(s, msg, server_addr)
+        recvresp(s)
 
-        #message logging
-        print "\n------sending------"
-        print msg
-        print "-------------------\n"
-        #message logging
-
-        s.sendto(msg, (recipient, PORT))
-
-        response = s.recv(1024)
-
-        #message logging
-        print "\n-----recieved------"
-        print response 
-        print "-------------------\n"
-        #message logging
     elif (command == "g"): # GRAB COMMAND: ask server for file location, then try to get file
-        msg = "whereis\n" + raw_input("what filename do you want to get")
-        recipient = server_addr
+        msg = "whereis\n" + raw_input("what filename do you want to get? ")
+        sendthis(s, msg, server_addr)
+        recvresp(s)
 
-        #message logging
-        print "\n-----sending------"
-        print msg
-        print "-------------------\n"
-        #message logging
-
-        s.sendto(msg, (recipient, PORT))
-
-        response = s.recv(1024)
-
-        #message logging
-        print "\n-----recieved------"
-        print response 
-        print "-------------------\n"
-        #message logging
     elif (command == "u"): # UPDATE COMMAND: update this user's shared files
         print "\nAll files in your drawer will be shared with the cabinet directory.\n"
-        files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
-        
-        filestring=""
-        for f in files:
-            filestring += f + "\\"
 
+        filestring = collectFiles()
         print "your files: ", filestring
+
         raw_input("\nwhen you're ready to share, press enter...")
-
-        
-        msg = "ihave\n" + filestring + "/"
-        recipient = server_addr
-
-        #message logging
-        print "\n-----sending------"
-        print msg
-        print "-------------------\n"
-        #message logging
-
-        s.sendto(msg, (recipient, PORT))
-
-        response = s.recv(1024)
-
-        #message logging
-        print "\n-----recieved------"
-        print response 
-        print "-------------------\n"
-        #message logging
+        sendthis(s, "ihave\n" + filestring, server_addr)
+        recvresp(s)
 
 s.close()
 
+def sendthis(s, msg, recipient):
+    PORT = 1998
+    #message logging
+    print "\n------sending------"
+    print msg
+    print "-------------------\n"
+    #message logging
+    s.sendto(msg, (recipient, PORT))
+
+def recvresp(s):
+    response,addr = s.recvfrom(1024)
+    #message logging
+    print "\n-----recieved------"
+    print response 
+    print "-------------------\n"
+    #message logging
+    return addr
+
+def collectFiles():
+    files = [f for f in listdir("MyDrawer/") if isfile(join("MyDrawer/", f))]
+
+    fileString=""
+
+    for f in files:
+        f += fileString + "\n"
+
+    return fileString
--- server/server.py
+++ server/server.py
@@ -11,6 +11,25 @@ while True:
     print
     print "\n* RECIEVED:\n", data  
     print "* FROM: ", addr
-    response = "YOU SAID:\n"+data
+
+    dataList = data.split("\n")
+
+    if (dataList[0]=="iam"):
+        # Register the user TODO
+        response = "message\nHello, "+dataList[1]+". Welcome to FilingCabinet."
+    elif (dataList[0]=="ihave"):
+        # Register the user's files TODO
+        response = "message\nI got your files. Thanks."
+    elif (dataList[0]=="doyouhave"):
+        # Check if I have that file on record TODO
+        response = "message\nchecking..."
+    elif (dataList[0]=="list"):
+        # Send the list of files TODO
+        response = "message\nHere's the list:\na\nb\nc"
+    elif (dataList[0]=="whereis"):
+        # Make sure I have that file TODO
+        # Get the file locationS TODO
+        response = "filelocations\na\nb\nc"
+        
     clientsock.sendto(response, addr)  
     print "* RESPONDED:\n", response
